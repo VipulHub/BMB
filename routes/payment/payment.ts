5f7bd228bb153f1945274e0ac131634c63ffeb52
aman <amanlamba817@gmail.com>
2026-01-21 09:18:33 +0530

    third commit v14

--- routes/payment/payment.ts
+++ routes/payment/payment.ts
@@ -1,7 +1,7 @@
 import { Router, type RequestHandler } from "express"
 import { emitter } from "../../utils/emiter.ts";
 import { createOrder } from "../../controller/index.ts";
-import { checkToken, validate } from "../../middleware/middleware.ts";
+import {  validate } from "../../middleware/middleware.ts";
 import { createOrderSchema } from "../../config/apiSchema/index.ts";
 
 const paymentRoutes = async (...middlewares: RequestHandler[]) => {
@@ -10,7 +10,7 @@ const paymentRoutes = async (...middlewares: RequestHandler[]) => {
         level: 'info'
     })
     const route = Router();
-    route.post('/createOrder', ...middlewares, validate(createOrderSchema), checkToken, createOrder)
+    route.post('/createOrder', ...middlewares, validate(createOrderSchema), createOrder)
     // route.get('/verifyPayment', ...middlewares, validate(), checkToken, signout)
     return route;
 }
